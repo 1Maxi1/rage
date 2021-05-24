53ebdd3cb570fe5c779288f8c2bdcc388e54170c
C4_lox <72700199+1Maxi1@users.noreply.github.com>
2021-05-24 17:04:32 +0300

    //Delete unnecessary from the code

--- packages/server/events/playerChat.js
+++ packages/server/events/playerChat.js
@@ -1,14 +1,3 @@
-//СТАРЫЙ КОД ЧАТА
-
-/*
-mp.events.add("playerChat", (player, text) =>
-{
-	mp.players.broadcast("<span style='color:#38e09f'>(" + player.id + ") " + player.name + "</span>: <span style='color:#ffffff'>" + text + "</span>");
-	console.log("<CHAT LOG>" + " (" + player.id + ") " + player.name + ": " + text);
-});*/
-
-//НОВЫЙ КОД ЧАТА
-
 mp.events.add('playerChat', (player, message) => {
     let pos = player.position;
     mp.players.broadcastInRange(pos, 15,`!{f1f1f1}${player.name} (${player.id}) сказал: ${message}`);
