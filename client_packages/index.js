b4a51eaeca006e8a9714f14e04014cda98dbebac
C4_lox <72700199+1Maxi1@users.noreply.github.com>
2022-01-29 18:14:12 +0300

    //fixed small bugs

--- client_packages/index.js
+++ client_packages/index.js
@@ -8,6 +8,6 @@ mp.events.add('showhud', () => {
 	hud = mp.browsers.new('package://ui/cef/test.html');
 })
 
-mp.discord.update('Играет на', 'SWG RP');//АКТИВНОСТЬ В ДС
+mp.discord.update('Играет на', 'RENAME');//АКТИВНОСТЬ В ДС
 mp.game.controls.disableControlAction(2, 243, true); //УБЕРАЕТ ЧИТ-КОДЫ
-mp.game.gxt.set("PM_PAUSE_HDR", "SWG RP | Новая политека"); //ТЕКСТ НАД КАРТОЙ В ESC
+mp.game.gxt.set("PM_PAUSE_HDR", "RENAME | Новая политека"); //ТЕКСТ НАД КАРТОЙ В ESC
